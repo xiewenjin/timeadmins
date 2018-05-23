229980b7c4a92c17ba79aa527e2afc03f65b4350
xiewenjin <254584437@qq.com>
2018-05-23 11:00:50 +0800

    修改

--- utils/api.js
+++ utils/api.js
@@ -22,7 +22,7 @@ function ajax1(data = '', fn, method = "get", header = {}) {
         'img': "@image('200x100', '#4A7BF7','#fff','pic')",
         'title': '@ctitle(3,8)',
         'city': "@county(true)",
-        'stock_num': '@integer(0,100)',//库存数量  
+        'stock_num': '@integer(0,100)',//库存数量  //ssss
         'marketing_start': '@datetime()',
         'marketing_stop': '@now()',
         'price': '@integer(100,2000)',//现价，单位：分  
